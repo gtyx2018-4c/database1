1974003e230511d367903be000283aebf46ff6cc
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2024-06-24 23:06:54 +0800

    Add files via upload

--- archives/original.js
+++ archives/original.js
@@ -1,6 +1,6 @@
 var db={Name10:"何恩琳",Number10:"10",Name14:"李承泽",Number14:"14",Name19:"李梓维",Number19:"19",Name21:"鲁米诺",Number21:"21",Name23:"欧锦欣",Number23:"23",Name25:"沈子涵",Number25:"25",Name28:"王瑾琦",Number28:"28",Name29:"王靖禧",Number29:"29",Name3:"陈钰儿",Number3:"3",Name31:"王曦田",Number31:"31",Name34:"夏若溪",Number34:"34",Name39:"尹诗贝",Number39:"39",Name40:"袁炜林",Number40:"40",Name41:"詹雅珺",Number41:"41",NameJSON解析失败:"JSON解析失败",NumberJSON解析失败:"JSON解析失败",Name7:"高天",Number7:"7",Name9:"郭佳茵",Number9:"9"};
 function getLen(){
-  return 17;
+  return 46;
 }
 function getDB(){
   return db;
